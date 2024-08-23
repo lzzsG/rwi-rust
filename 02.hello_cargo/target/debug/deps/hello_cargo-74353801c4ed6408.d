115fdffd245c1dee89166202c90794b59660abf8
Lzzs <147058959+lzzsG@users.noreply.github.com>
2024-08-23 15:25:07 +0800

    Delete 02.hello_cargo/target directory

--- 02.hello_cargo/target/debug/deps/hello_cargo-74353801c4ed6408.d
+++
@@ -1,5 +0,0 @@
-/Users/liuzhizhi/Code/rwi-rust/02.hello_cargo/target/debug/deps/libhello_cargo-74353801c4ed6408.rmeta: src/main.rs
-
-/Users/liuzhizhi/Code/rwi-rust/02.hello_cargo/target/debug/deps/hello_cargo-74353801c4ed6408.d: src/main.rs
-
-src/main.rs:
--- 02.hello_cargo/target/release/deps/hello_cargo-3d5eb247b9fa7064.d
+++
@@ -1,5 +0,0 @@
-/Users/liuzhizhi/Code/rwi-rust/02.hello_cargo/target/release/deps/hello_cargo-3d5eb247b9fa7064: src/main.rs
-
-/Users/liuzhizhi/Code/rwi-rust/02.hello_cargo/target/release/deps/hello_cargo-3d5eb247b9fa7064.d: src/main.rs
-
-src/main.rs:
